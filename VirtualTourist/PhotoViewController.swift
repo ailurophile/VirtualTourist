4a0baeba8c2cb5e6df3ee73eab0c9fcb3123dca4
ailurophile <lisalitchfield@aim.com>
2017-03-12 20:44:30 -0700

    feat: Create Photo view controller with map view

---
+++ VirtualTourist/PhotoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoViewController.swift
+//  VirtualTourist
+//
+//  Created by Lisa Litchfield on 3/12/17.
+//  Copyright © 2017 Lisa Litchfield. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class PhotoViewController: UICollectionViewController, MKMapViewDelegate {
+    
+}
+
